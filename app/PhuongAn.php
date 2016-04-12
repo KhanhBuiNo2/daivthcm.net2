3d65c7fcce890c9f4043f895a6667897b2601bac
KhanhBuiNo2 <khanhBuiNo2@gmail.com>
2016-04-12 10:45:08 +0700

    test

--- app/PhuongAn.php
+++ app/PhuongAn.php
@@ -9,10 +9,21 @@ use App\Model\PhuongAn;
 use App\Model\PhuongAnDepartment;
 use App\Model\PhuongAnStatus;
 use DB;
+use App\User;
 /*
 **
 */
 class PhuongAn extends Model
 {	
-	
+	public static function getPhuongAnFromUserID($UserID){
+		$user = User::find($UserID) -> toArray();
+		if(!empty($user)){
+			$DepartmentID = $user['DepartmentID'];
+			return DB::table('phuongan_department')
+							-> join('phuongan','phuongan.PhuongAnID','=','phuongan_department.PhuongAnID')
+							-> join('phuongan_status','phuongan_status.PhuongAnID','=','phuongan_department.PhuongAnID')
+							-> where('phuongan_department.PhuongAnID','=',$DepartmentID)
+							-> get();
+		}
+	}
 }
